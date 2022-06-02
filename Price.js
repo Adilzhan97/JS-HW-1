1754051514aaa42d849c333e07c829e2489c082a
Adilzhan <105296638+Adilzhan97@users.noreply.github.com>
2022-06-02 14:06:31 +0600

    Add files via upload

---
+++ Price.js
@@ -0,0 +1,12 @@
+let price = 0;
+
+while(price < 30) {
+    if(price < 10) {
+    console.log("price =",price , "Small")
+}   else if (price > 20) {
+    console.log("price =",price , "Big")
+}    else(console.log("price =",price, "Normal"))
+price ++
+}
+
+
